56c4b15be6186589ca6014bac88681db4859a448
mcbplay1@gmail.com <mcbplay1@gmail.com>
2021-03-23 14:32:32 +0000

    fix

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
+read -p 'Link: ' LINK
 UNIX="&fetch=cooked&format=powersfx&container=unix"
-URL="$1$UNIX"
+URL="$LINK$UNIX"
 wget --no-check-certificate "$URL" -O unpack.zip
 unzip unpack.zip
 ./RunMe.sh
 ffmpeg -i craig.flac -ab 320k -map_metadata 0 -id3v2_version 3 craig.mp3
 mv craig.mp3 out/
+
